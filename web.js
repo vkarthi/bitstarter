2bcae27cc788f4c820a13bad9e218e5615c0e5ac
Vadivel Karthikeyan <vmkar96@gmail.com>
2013-07-10 16:13:00 +0000

    modified web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+fs = require('fs');
+fs.readFile('./index.html', 'utf8', function (err,data) {
+if (err) {
+   return console.log(err);
+  }
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
